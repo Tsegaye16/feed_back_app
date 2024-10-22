03ac54e7e82d104e097b53a04d6896d86193962e
Tsegaye16 <abewatsegaye16@gmail.com>
2024-10-22 19:46:03 +0300

    installing sharp

--- server/db.js
+++ server/db.js
@@ -6,10 +6,12 @@ export const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER || "feed_back_user",
   process.env.DB_PASSWORD,
+
   {
     host: process.env.DB_HOST, // Just the hostname (without the 'postgresql://')
     port: process.env.DB_PORT || 5432,
     dialect: "postgres",
+    logging: false,
     dialectOptions: {
       ssl: {
         require: true,
