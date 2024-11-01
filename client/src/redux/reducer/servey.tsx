e6d9d4e91110917eea464c8788cf366889d834de
Tsegaye16 <abewatsegaye16@gmail.com>
2024-11-01 14:58:11 +0300

    refactoring

--- client/src/redux/reducer/servey.tsx
+++ client/src/redux/reducer/servey.tsx
@@ -1,4 +1,3 @@
-// Importing action type constants
 import {
   GET_ALL_SERVEY,
   ADD_SERVEY,
@@ -7,46 +6,22 @@ import {
   PUBLISH_SURVEY,
 } from "../../constants/types/actionType";
 
-// Define the shape of a Survey (example, adjust according to your actual data)
-interface Survey {
-  id: string;
-  name: string;
-  secretePhrase: string;
-  isPublished: boolean;
-  // Add other fields as necessary
-}
-
-// Define the shape of the state
-interface SurveyState {
-  survey: Survey | null;
-}
-
-// Define the action types
-interface Action<T = any> {
-  type: string;
-  payload?: T;
-}
-
-// Initial state, with correct type
-const initialState: SurveyState = {
+const serveyState = {
   survey: null,
 };
 
-// Survey reducer with type safety
-const serveyReducer = (
-  state: SurveyState = initialState,
-  action: Action
-): SurveyState => {
+const serveyReducer = (state = serveyState, action: any) => {
   switch (action.type) {
     case ADD_SERVEY:
+      return { ...state, servey: action.payload };
     case GET_ALL_SERVEY:
+      return { ...state, servey: action.payload };
     case DELETE_SERVEY:
-      return { ...state, survey: action.payload as Survey };
-
+      return { ...state, servey: action.payload };
     case GET_FULL_SURVEY:
+      return { ...state, survey: action.payload };
     case PUBLISH_SURVEY:
-      return { ...state, survey: action.payload as Survey };
-
+      return { ...state, survey: action.payload };
     default:
       return state;
   }
