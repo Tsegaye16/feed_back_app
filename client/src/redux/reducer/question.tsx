6ef1f27938437741a7b63b87c4f5859df6b752b2
Tsegaye16 <abewatsegaye16@gmail.com>
2024-11-06 15:09:57 +0300

    Refactoring

--- client/src/redux/reducer/question.tsx
+++ client/src/redux/reducer/question.tsx
@@ -1,6 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 import {
-
   addQuestion,
   deleteQuestionById,
   getAllQuestion,
@@ -8,7 +7,6 @@ import {
   sortQuestion,
   updateQuestion,
 } from "../action/company";
-
 
 const questionSlice = createSlice({
   name: "question",
@@ -76,7 +74,7 @@ const questionSlice = createSlice({
   },
 });
 
-
+export default questionSlice.reducer;
 
 // const questionReducer = (state = questionState, action: any) => {
 //   switch (action.type) {
