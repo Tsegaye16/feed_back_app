a25d0c0511a7a4f33f693c9d48e3ae1875858d20
Tsegaye16 <abewatsegaye16@gmail.com>
2025-04-12 17:23:30 +0300

    chatbot

--- server/db.js
+++ server/db.js
@@ -13,10 +13,10 @@ export const sequelize = new Sequelize(
     dialect: "postgres",
     logging: false,
     dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+      // ssl: {
+      //   require: true,
+      //   rejectUnauthorized: false,
+      // },
     },
   }
 );
