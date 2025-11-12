e12d7a8bde6f819c80d432f7a4281dcfbd18dcfa
Tsegaye16 <abewatsegaye16@gmail.com>
2025-11-12 23:26:56 +0300

    updating ssl

--- server/db.js
+++ server/db.js
@@ -1,29 +1,22 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "./.env" });
-
-const sslRequired = String(process.env.DB_SSL).toLowerCase() === "true";
-
-const sequelizeOptions = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
-  dialect: "postgres",
-  logging: false,
-};
-
-if (sslRequired) {
-  sequelizeOptions.dialectOptions = {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  };
-}
+dotenv.config();
 
 export const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  sequelizeOptions
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
 );
